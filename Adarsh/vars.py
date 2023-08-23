3e0a968ec6b6da373fe433b2ae9402d764dd44bc
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-08-23 21:20:56 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,22 +9,22 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '14505719'))
-    API_HASH = str(getenv('API_HASH', '620f0a2aa2cd1474a4953619b3e3643d'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6126205588:AAEpoJttxR56CI9ubvy7P6O89ENjD7aGqWU'))
+    API_ID = int(getenv('API_ID', '24865057'))
+    API_HASH = str(getenv('API_HASH', 'a45372ac22649e134c7871ab3125bfb1'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6415159418:AAFhfA3au-E3lIEo6JeDW2Qi2GRJpDYcGxs'))
     SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkda'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001659362808'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001788767433'))
     PORT = int(getenv('PORT', 8080))
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1893496887").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5557955876").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     SHORTENER_API = str(getenv('SHORTENER_API', '4fbe384f585aa6c0df736bd9a859bfc3b9216fee'))
     SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnlink.in'))
-    FDQN = str(getenv('FDQN', 'https://filetolinkda.onrender.com'))
+    FDQN = str(getenv('FDQN', ''))
 
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
