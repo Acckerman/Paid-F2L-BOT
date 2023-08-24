ac322ec6af9d401ce397fd870696257bd85348c5
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-08-24 19:21:41 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -40,6 +40,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://Haashim:Haashim@mfile0.t9hxg.mongodb.net/?retryWrites=true&w=majority"))
+    DATABASE_URL = str(getenv('DATABASE_URL', ""))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@tamilanmovieschat"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
